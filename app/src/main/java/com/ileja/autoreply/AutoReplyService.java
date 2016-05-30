0810dcacb338adfc766bbf4553eb9aa6acb452f1
tao.chen <tao.chen@carrobot.com>
2016-05-30 23:02:05 +0800

    notify when lock screen

--- app/src/main/java/com/ileja/autoreply/AutoReplyService.java
+++ app/src/main/java/com/ileja/autoreply/AutoReplyService.java
@@ -3,23 +3,35 @@ package com.ileja.autoreply;
 import android.accessibilityservice.AccessibilityService;
 import android.annotation.SuppressLint;
 import android.app.ActivityManager;
+import android.app.KeyguardManager;
 import android.app.Notification;
 import android.app.PendingIntent;
 import android.content.ClipData;
 import android.content.ClipboardManager;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.PowerManager;
+import android.text.TextUtils;
 import android.view.accessibility.AccessibilityEvent;
 import android.view.accessibility.AccessibilityNodeInfo;
 
 import java.util.List;
 
 public class AutoReplyService extends AccessibilityService {
+    private final static String MM_PNAME = "com.tencent.mm";
     boolean hasAction = false;
+    boolean locked = false;
+    boolean background = false;
+    private KeyguardManager.KeyguardLock kl;
+    private Handler handler = new Handler();
+
     @Override
-    public void onAccessibilityEvent(AccessibilityEvent event) {
+    public void onAccessibilityEvent(final AccessibilityEvent event) {
         int eventType = event.getEventType();
+        android.util.Log.d("maptrix", "get event = " + eventType);
         switch (eventType) {
             case AccessibilityEvent.TYPE_NOTIFICATION_STATE_CHANGED:// 通知栏事件
                 android.util.Log.d("maptrix", "get notification event");
@@ -28,17 +40,48 @@ public class AutoReplyService extends AccessibilityService {
                     for (CharSequence text : texts) {
                         String content = text.toString();
                         if (content.contains("777")) {
-                            // 监听到微信红包的notification，打开通知
-                            hasAction = true;
-                            if (event.getParcelableData() != null
-                                    && event.getParcelableData() instanceof Notification) {
-                                Notification notification = (Notification) event
-                                        .getParcelableData();
-                                PendingIntent pendingIntent = notification.contentIntent;
-                                try {
-                                    pendingIntent.send();
-                                } catch (PendingIntent.CanceledException e) {
-                                    e.printStackTrace();
+                            if (isScreenLocked()) {
+                                locked = true;
+                                wakeAndUnlock();
+                                android.util.Log.d("maptrix", "the screen is locked");
+                                if (isAppForeground(MM_PNAME)) {
+                                    background = false;
+                                    android.util.Log.d("maptrix", "is mm in foreground");
+                                    sendNotifacationReply(event);
+                                    handler.postDelayed(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            sendNotifacationReply(event);
+                                            if (fill()) {
+                                                send();
+                                            }
+                                        }
+                                    }, 1000);
+                                } else {
+                                    background = true;
+                                    android.util.Log.d("maptrix", "is mm in background");
+                                    sendNotifacationReply(event);
+                                }
+                            } else {
+                                locked = false;
+                                android.util.Log.d("maptrix", "the screen is unlocked");
+                                // 监听到微信红包的notification，打开通知
+                                if (isAppForeground(MM_PNAME)) {
+                                    background = false;
+                                    android.util.Log.d("maptrix", "is mm in foreground");
+                                    sendNotifacationReply(event);
+                                    handler.postDelayed(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            if (fill()) {
+                                                send();
+                                            }
+                                        }
+                                    }, 1000);
+                                } else {
+                                    background = true;
+                                    android.util.Log.d("maptrix", "is mm in background");
+                                    sendNotifacationReply(event);
                                 }
                             }
                         }
@@ -46,17 +89,18 @@ public class AutoReplyService extends AccessibilityService {
                 }
                 break;
             case AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED:
-                android.util.Log.d("maptrix", "get type windon down event");
-                if(!hasAction)break;
+                android.util.Log.d("maptrix", "get type window down event");
+                if (!hasAction) break;
                 String className = event.getClassName().toString();
                 if (className.equals("com.tencent.mm.ui.LauncherUI")) {
-                    if(fill()){
+                    if (fill()) {
                         send();
                     }
                 }
 
                 //bring2Front();
                 back2Home();
+                release();
                 hasAction = false;
                 break;
         }
@@ -68,14 +112,14 @@ public class AutoReplyService extends AccessibilityService {
         if (nodeInfo != null) {
             List<AccessibilityNodeInfo> list = nodeInfo
                     .findAccessibilityNodeInfosByText("发送");
-            if(list != null && list.size()>0) {
+            if (list != null && list.size() > 0) {
                 for (AccessibilityNodeInfo n : list) {
                     n.performAction(AccessibilityNodeInfo.ACTION_CLICK);
                 }
-            }else{
+            } else {
                 List<AccessibilityNodeInfo> liste = nodeInfo
                         .findAccessibilityNodeInfosByText("Send");
-                if(liste != null&&liste.size()>0) {
+                if (liste != null && liste.size() > 0) {
                     for (AccessibilityNodeInfo n : liste) {
                         n.performAction(AccessibilityNodeInfo.ACTION_CLICK);
                     }
@@ -83,6 +127,21 @@ public class AutoReplyService extends AccessibilityService {
             }
         }
 
+    }
+
+    private void sendNotifacationReply(AccessibilityEvent event) {
+        hasAction = true;
+        if (event.getParcelableData() != null
+                && event.getParcelableData() instanceof Notification) {
+            Notification notification = (Notification) event
+                    .getParcelableData();
+            PendingIntent pendingIntent = notification.contentIntent;
+            try {
+                pendingIntent.send();
+            } catch (PendingIntent.CanceledException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     @SuppressLint("NewApi")
@@ -95,12 +154,18 @@ public class AutoReplyService extends AccessibilityService {
         return false;
     }
 
-    private boolean findEditText(AccessibilityNodeInfo rootNode, String content){
+    private boolean findEditText(AccessibilityNodeInfo rootNode, String content) {
         int count = rootNode.getChildCount();
-        for(int i = 0; i < count; i++){
+        android.util.Log.d("maptrix", "root class=" + rootNode.getClassName() + ","+ rootNode.getText()+","+count);
+        for (int i = 0; i < count; i++) {
             AccessibilityNodeInfo nodeInfo = rootNode.getChild(i);
-            android.util.Log.d("maptrix", "class="+nodeInfo.getClassName());
-            if("android.widget.EditText".equals(nodeInfo.getClassName())){
+            if (nodeInfo == null) {
+                android.util.Log.d("maptrix", "nodeinfo = null");
+                continue;
+            }
+
+            android.util.Log.d("maptrix", "class=" + nodeInfo.getClassName());
+            if ("android.widget.EditText".equals(nodeInfo.getClassName())) {
                 android.util.Log.d("maptrix", "==================");
                 Bundle arguments = new Bundle();
                 arguments.putInt(AccessibilityNodeInfo.ACTION_ARGUMENT_MOVEMENT_GRANULARITY_INT,
@@ -110,14 +175,14 @@ public class AutoReplyService extends AccessibilityService {
                 nodeInfo.performAction(AccessibilityNodeInfo.ACTION_PREVIOUS_AT_MOVEMENT_GRANULARITY,
                         arguments);
                 nodeInfo.performAction(AccessibilityNodeInfo.ACTION_FOCUS);
-                ClipData clip = ClipData.newPlainText("label", content );
-                ClipboardManager clipboardManager = (ClipboardManager)getSystemService(Context.CLIPBOARD_SERVICE);
+                ClipData clip = ClipData.newPlainText("label", content);
+                ClipboardManager clipboardManager = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
                 clipboardManager.setPrimaryClip(clip);
                 nodeInfo.performAction(AccessibilityNodeInfo.ACTION_PASTE);
                 return true;
             }
 
-            if(findEditText(nodeInfo, content)){
+            if (findEditText(nodeInfo, content)) {
                 return true;
             }
         }
@@ -130,24 +195,88 @@ public class AutoReplyService extends AccessibilityService {
 
     }
 
+    /**
+     * 判断指定的应用是否在前台运行
+     *
+     * @param packageName
+     * @return
+     */
+    private boolean isAppForeground(String packageName) {
+        ActivityManager am = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        ComponentName cn = am.getRunningTasks(1).get(0).topActivity;
+        String currentPackageName = cn.getPackageName();
+        if (!TextUtils.isEmpty(currentPackageName) && currentPackageName.equals(packageName)) {
+            return true;
+        }
 
-    private void bring2Front(){
-        ActivityManager activtyManager = (ActivityManager)getSystemService(Context.ACTIVITY_SERVICE);
+        return false;
+    }
+
+
+    /**
+     * 将当前应用运行到前台
+     */
+    private void bring2Front() {
+        ActivityManager activtyManager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
         List<ActivityManager.RunningTaskInfo> runningTaskInfos = activtyManager.getRunningTasks(3);
-        for(ActivityManager.RunningTaskInfo runningTaskInfo : runningTaskInfos){
-            if(this.getPackageName().equals(runningTaskInfo.topActivity.getPackageName())){
+        for (ActivityManager.RunningTaskInfo runningTaskInfo : runningTaskInfos) {
+            if (this.getPackageName().equals(runningTaskInfo.topActivity.getPackageName())) {
                 activtyManager.moveTaskToFront(runningTaskInfo.id, ActivityManager.MOVE_TASK_WITH_HOME);
                 return;
             }
         }
     }
 
-    private void back2Home(){
+    /**
+     * 回到系统桌面
+     */
+    private void back2Home() {
         Intent home = new Intent(Intent.ACTION_MAIN);
 
         home.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         home.addCategory(Intent.CATEGORY_HOME);
 
         startActivity(home);
+    }
+
+
+    /**
+     * 系统是否在锁屏状态
+     *
+     * @return
+     */
+    private boolean isScreenLocked() {
+        KeyguardManager keyguardManager = (KeyguardManager) getSystemService(Context.KEYGUARD_SERVICE);
+        return keyguardManager.inKeyguardRestrictedInputMode();
+    }
+
+    private void wakeAndUnlock() {
+        //获取电源管理器对象
+        PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
+
+        //获取PowerManager.WakeLock对象，后面的参数|表示同时传入两个值，最后的是调试用的Tag
+        PowerManager.WakeLock wl = pm.newWakeLock(PowerManager.ACQUIRE_CAUSES_WAKEUP | PowerManager.SCREEN_BRIGHT_WAKE_LOCK, "bright");
+
+        //点亮屏幕
+        wl.acquire(1000);
+
+        //得到键盘锁管理器对象
+        KeyguardManager km = (KeyguardManager) getSystemService(Context.KEYGUARD_SERVICE);
+        kl = km.newKeyguardLock("unLock");
+
+        //解锁
+        kl.disableKeyguard();
+
+    }
+
+    private void release() {
+
+        if (locked && kl != null) {
+            android.util.Log.d("maptrix", "release the lock");
+            //得到键盘锁管理器对象
+            kl.reenableKeyguard();
+
+            locked = false;
+        }
     }
 }
--- app/src/main/java/com/ileja/autoreply/MainActivity.java
+++ app/src/main/java/com/ileja/autoreply/MainActivity.java
@@ -1,13 +1,39 @@
 package com.ileja.autoreply;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.KeyguardManager;
+import android.content.Context;
 import android.os.Bundle;
+import android.os.PowerManager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
+    private PowerManager.WakeLock wakeLock;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //wakeDevice();
+    }
+
+    public void wakeDevice() {
+        PowerManager powerManager = (PowerManager) getSystemService(Context.POWER_SERVICE);
+        wakeLock = powerManager.newWakeLock((PowerManager.SCREEN_BRIGHT_WAKE_LOCK | PowerManager.FULL_WAKE_LOCK | PowerManager.ACQUIRE_CAUSES_WAKEUP), "TAG");
+        wakeLock.acquire();
+
+        KeyguardManager keyguardManager = (KeyguardManager) getSystemService(Context.KEYGUARD_SERVICE);
+        KeyguardManager.KeyguardLock keyguardLock = keyguardManager.newKeyguardLock("TAG");
+        keyguardLock.disableKeyguard();
+        runOnUiThread(new Runnable(){
+            public void run(){
+                getWindow().addFlags(
+                        WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED
+                                | WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON
+                                | WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON
+                                | WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD);
+            }
+        });
     }
 }
